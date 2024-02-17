2f7fdc5250b46a32d1f7a24aaf7db8c3219fc2b4
Master <Master@host.docker.internal>
2024-02-17 13:27:48 +0200

    Commit only Post

--- src/main/java/telran/java51/account/dao/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package telran.java51.account.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import telran.java51.account.model.Account;
-
-public interface AccountRepository  extends CrudRepository<Account, String>{
-		
-}
